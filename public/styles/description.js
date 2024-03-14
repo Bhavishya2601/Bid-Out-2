1cc830a86c8ab48eb289e1301d6dcc85633fc316
bhavishya2601 <bgarg2050bhav@gmail.com>
2024-03-14 23:20:02 +0530

    all pages

---
+++ public/styles/description.js
@@ -0,0 +1,99 @@
+// speak aloud
+let isReading = false;
+let speech;
+
+const text = document.getElementById('text').innerText;
+const readButton = document.getElementById('readButton');
+
+readButton.addEventListener('click', () => {
+    if (!isReading) {
+        speech = new SpeechSynthesisUtterance();
+        speech.text = text;
+        speech.volume = 1;
+        speech.rate = 1;
+        speech.pitch = 1;
+        speech.voice = speechSynthesis.getVoices().find(voice => voice.name === 'Google UK English Female'); // You may need to adjust this voice name based on available options
+        window.speechSynthesis.speak(speech);
+        isReading = true;
+        readButton.innerText = "Stop Reading";
+    } else {
+        window.speechSynthesis.cancel();
+        isReading = false;
+        readButton.innerText = "Read Text";
+    }
+});
+
+
+
+// for bidding
+let currentNumber = parseInt(localStorage.getItem('currentNumber')) || parseInt(document.getElementById('output1').innerHTML);
+if (!isNaN(currentNumber)) {
+    document.getElementById('output1').innerText = `$${currentNumber}`;
+}
+
+document.getElementById('guessForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    const guess = parseInt(document.getElementById('guessInput').value);
+
+    // console.log(guess, currentNumber)
+    if (guess > currentNumber) {
+        currentNumber = guess;
+        alert('Your Bid has been Successfully Placed.')
+        document.getElementById('output1').innerText = `$${currentNumber}`;
+        localStorage.setItem('currentNumber', currentNumber);
+    } else {
+        alert('Bid amount can\'t be less than or equal to current bid');
+    }
+
+    // Reset the form
+    document.getElementById('guessInput').value = '';
+});
+
+// to change the slides
+let slideIndex = 1;
+        showSlides(slideIndex);
+
+        function plusSlides(n) {
+        showSlides(slideIndex += n);
+        }
+
+        function currentSlide(n) {
+        showSlides(slideIndex = n);
+        }
+
+        function showSlides(n) {
+        let i;
+        let slides = document.getElementsByClassName("mySlides");
+        let dots = document.getElementsByClassName("card");
+        let captionText = document.getElementById("caption");
+        if (n > slides.length) {slideIndex = 1}
+        if (n < 1) {slideIndex = slides.length}
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex-1].style.display = "block";
+        dots[slideIndex-1].className += " active";
+        captionText.innerHTML = dots[slideIndex-1].alt;
+        }
+
+
+// for description part
+function showDescription() {
+    document.getElementById('descriptionSection').style.display = 'block';
+    document.getElementById('bidProduct').style.display = 'none';
+    document.getElementById('bidReview').style.display = 'none';
+}
+
+function showProducts() {
+    document.getElementById('descriptionSection').style.display = 'none';
+    document.getElementById('bidReview').style.display = 'none';
+    document.getElementById('bidProduct').style.display = 'block';
+}
+function showReview(){
+    document.getElementById('descriptionSection').style.display = 'none';
+    document.getElementById('bidReview').style.display = 'block';
+    document.getElementById('bidProduct').style.display = 'none';
+}
--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -64,14 +64,14 @@ export default function Profile() {
           </div>
           <div class="flex flex-col items-center -mt-20">
            {listings[0]?.data?.images.length>0 ? <img
-              src={listings[0]?.data?.images[0]}
+              src={auth.currentUser.displaylocation}
               class="w-40 h-40 border-4 border-white rounded-full"
             />: <img
             src={defaultImage}
             class="w-40 h-40 border-4 border-white rounded-full"
           />}
             <div class="flex items-center space-x-2 mt-2">
-              <p class="text-2xl">{listings[0]?.data?.name}</p>
+              <p class="text-2xl">{auth.currentUser.displayName}</p>
               <span class="bg-blue-500 rounded-full p-1" title="Verified">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -90,25 +90,13 @@ export default function Profile() {
               </span>
             </div>
             <p class="text-gray-700 text-center">
-              {listings[0]?.data?.position} at BID OUT
+            {auth.currentUser.displayemail} at BID OUT
             </p>
             <p class="text-sm text-gray-500">{listings[0]?.data?.location}</p>
           </div>
           <div class="flex-1 flex flex-col items-center lg:items-end justify-end px-8 mt-2">
             <div class="flex items-center space-x-4 mt-2">
-              <button onClick={()=>{auth.signOut()
-                navigate("/")}} class="flex items-center bg-red-600 hover:bg-red-700 text-gray-100 px-4 py-2 rounded text-sm space-x-2 transition duration-100">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="h-4 w-4"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path d="M8 9a3 3 0 100-6 3 3 0 000 6zM8 11a6 6 0 016 6H2a6 6 0 016-6zM16 7a1 1 0 10-2 0v1h-1a1 1 0 100 2h1v1a1 1 0 102 0v-1h1a1 1 0 100-2h-1V7z"></path>
-                </svg>
-                <span className="text-xs xs:text-sm">Logout</span>
-              </button>
-              <a href="mailto:darklegend912005@gmail.com" class="flex items-center bg-blue-600 hover:bg-blue-700 text-gray-100 px-4 py-2 rounded text-sm space-x-2 transition duration-100">
+              <a href="mailto:darklegend912005@gmail.com" class="flex items-center bg-blue-600 hover:bg-green-700 text-gray-100 px-4 py-2 rounded text-sm space-x-2 transition duration-100">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   class="h-4 w-4"
@@ -121,8 +109,35 @@ export default function Profile() {
                     clip-rule="evenodd"
                   ></path>
                 </svg>
-                <span className="text-xs xs:text-sm">Get in Touch</span>
+                <span className="text-xs xs:text-sm">Get In Contact</span>
               </a>
+              <a  href="../../public/pages/front-account.html"class="flex items-center bg-blue-600 hover:bg-green-700 text-gray-100 px-4 py-2 rounded text-sm space-x-2 transition duration-100">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-4 w-4"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M18 5v8a2 2 0 01-2 2h-5l-5 4v-4H4a2 2 0 01-2-2V5a2 2 0 012-2h12a2 2 0 012 2zM7 8H5v2h2V8zm2 0h2v2H9V8zm6 0h-2v2h2V8z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+                <span className="text-xs xs:text-sm">Continue to Homepage</span>
+              </a>
+              <button onClick={()=>{auth.signOut()
+                navigate("/")}} class="flex items-center bg-pink-600 hover:bg-red-700 text-gray-100 px-4 py-2 rounded text-sm space-x-2 transition duration-100">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-4 w-4"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path d="M8 9a3 3 0 100-6 3 3 0 000 6zM8 11a6 6 0 016 6H2a6 6 0 016-6zM16 7a1 1 0 10-2 0v1h-1a1 1 0 100 2h1v1a1 1 0 102 0v-1h1a1 1 0 100-2h-1V7z"></path>
+                </svg>
+                <span className="text-xs xs:text-sm">Logout</span>
+              </button>
             </div>
           </div>
         </div>
@@ -278,7 +293,6 @@ export default function Profile() {
                 </li>
               </ul>
             </div>
-            {/*  */}
           </div>
           <div class="flex flex-col w-full 2xl:w-2/3">
             <div class="flex-1 bg-white rounded-lg shadow-xl p-8 flex flex-col items-start justify-start w-full">
